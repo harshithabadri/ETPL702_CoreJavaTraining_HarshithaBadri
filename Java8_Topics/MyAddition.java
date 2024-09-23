2ba5d1e8854d0f49e18e8d4dd1b011b3e884828b
harshithabadri <harshithabadri2848@gmail.com>
2024-09-23 11:19:33 +0530

    Add files via upload

---
+++ Java8_Topics/MyAddition.java
@@ -0,0 +1,22 @@
+package com.evergent.java8.LambdaExpressions;
+
+ interface Add2
+ {
+	 public abstract  int addition(int a,int b);
+ }       
+
+public class MyAddition {
+	
+	 public   int addition(int a,int b) {
+		 return a+b;
+		 
+	 }
+
+	public static void main(String[] args) {
+		 
+		MyAddition m=new MyAddition();
+		
+		int sum =m.addition(2,3);
+		System.out.println(sum);
+		}
+}
